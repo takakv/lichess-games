96396310d0f96fe3e0cf5a44092c224642e83341
Taka <71265675+takakv@users.noreply.github.com>
2021-01-02 04:26:05 +0200

    Remove unnecessary stuff

--- plot_ratings.R
+++ plot_ratings.R
@@ -1,23 +1,14 @@
 ramen_ratings <- read.csv("ramen-ratings.csv")
 
-# Ratings total
-rating_vector <- ramen_ratings$Stars
-rating_vector <- as.numeric(rating_vector)
-rating_vector <- rating_vector[!is.na(rating_vector)]
-
-# Ratings per brand
-brand_vector <- subset(ramen_ratings, ramen_ratings$Brand)
-
-# Discrete frequency table of ratings
-factor(rating_vector)
-rating_freq <- table(rating_vector)
-barplot(rating_freq)
-
 # Convert dataset to usable form
 library(dplyr)
 processed_data <- ramen_ratings[, c(2,4,5,6)]
 processed_data$Stars <- as.numeric(processed_data$Stars)
 processed_data <- na.omit(processed_data)
+
+# Discrete frequency table of ratings
+rating_freq <- table(processed_data$Stars)
+barplot(rating_freq)
 
 # Mean of ratings per brand
 brand_freq <- group_by(processed_data, Brand) %>% summarize(mean = mean(Stars))
