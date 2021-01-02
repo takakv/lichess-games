32f1ae9af1832315b6275d64b34fa6120a2ef2f8
Taka <71265675+takakv@users.noreply.github.com>
2021-01-02 03:39:48 +0200

    Discrete frequency table

--- plot_ratings.R
+++ plot_ratings.R
@@ -2,5 +2,8 @@ ramen_ratings <- read.csv("ramen-ratings.csv")
 rating_vector <- ramen_ratings$Stars
 rating_vector <- as.numeric(rating_vector)
 rating_vector <- rating_vector[!is.na(rating_vector)]
-View(rating_vector)
-barplot(rating_vector)
+
+# Discrete frequency table
+factor(rating_vector)
+rating_freq <- table(rating_vector)
+barplot(rating_freq)
